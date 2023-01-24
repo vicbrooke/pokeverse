44523afc49ac4870035ad8d4e92025fa6f713aa0
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-01-24 11:56:58 +0000

    Started styling

--- src/routes/PokemonDetails.js
+++ src/routes/PokemonDetails.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 export default PokemonDetails = () => {
@@ -22,35 +23,58 @@ export default PokemonDetails = () => {
   return !pokemon ? (
     <>Loading...</>
   ) : (
-    <div data-testid="pokemon_details">
-      <h2>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
-      <img src={pokemon.sprites.front_default} />
-
-      <h3>Height: </h3>
-      <p>{pokemon.height}</p>
-      <h3>Weight: </h3>
-      <p>{pokemon.weight}</p>
-      <h3>
-        Abilities:
-        {pokemon.abilities.map((ability) => (
-          <h4 key={ability.ability.name}>{ability.ability.name}</h4>
-        ))}
-      </h3>
-      <h3>
-        Types:
-        {pokemon.types.map((type) => (
-          <h4 key={type.type.name}>{type.type.name}</h4>
-        ))}
-      </h3>
-      <h3>
-        Stats:{" "}
-        {pokemon.stats.map((stat) => (
-          <>
-            <h4 key={stat.stat.name}>{stat.stat.name}</h4>
-            <h6 key={stat.base_stat}>{stat.base_stat}</h6>
-          </>
-        ))}
-      </h3>
-    </div>
+    <Container data-testid="pokemon_details">
+      <Row>
+        <Col>
+          <h1>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={pokemon.sprites.front_default} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Height:</h2>
+          <h3>{pokemon.height}</h3>
+        </Col>
+        <Col>
+          <h2>Weight:</h2>
+          <h3>{pokemon.weight}</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>
+            Abilities:
+            {pokemon.abilities.map((ability) => (
+              <h3 key={ability.ability.name}>{ability.ability.name}</h3>
+            ))}
+          </h2>
+        </Col>
+        <Col>
+          <h2>
+            Types:
+            {pokemon.types.map((type) => (
+              <h3 key={type.type.name}>{type.type.name}</h3>
+            ))}
+          </h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>
+            Stats:{" "}
+            {pokemon.stats.map((stat) => (
+              <>
+                <h3 key={stat.stat.name}>{stat.stat.name}</h3>
+                <h3 key={stat.base_stat}>{stat.base_stat}</h3>
+              </>
+            ))}
+          </h2>
+        </Col>
+      </Row>
+    </Container>
   );
 };
